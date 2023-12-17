3fd3cccb321f58cf79f421d32b7061a940c7330e
Max Shyn <makschinasil@gmail.com>
2023-12-17 11:18:02 +0600

    exampleImageUrls field in ComplexImageTask HCaptcha

--- src/Requests/ComplexImageHCaptchaRequest.ts
+++ src/Requests/ComplexImageHCaptchaRequest.ts
@@ -1,10 +1,19 @@
 import { ComplexImageRequestBase, ComplexImageRequestBaseIn } from './ComplexImageRequestBase';
 import { TaskType } from '../TaskType';
 
-export type ComplexImageHCaptchaRequestIn = Pick<ComplexImageRequestBaseIn, Exclude<keyof ComplexImageRequestBaseIn, 'type' | '_class'>>;
+export type ComplexImageHCaptchaRequestIn = Pick<ComplexImageRequestBaseIn, Exclude<keyof ComplexImageRequestBaseIn, 'type' | '_class'>> & {
+  exampleImagesBase64?: Array<string>;
+};
 
 export class ComplexImageHCaptchaRequest extends ComplexImageRequestBase {
-  constructor(argsObjs: ComplexImageHCaptchaRequestIn) {
+  /**
+   * List of exampleImageUrls in base64
+   * It is required if hCaptcha is 3x3 grid
+   */
+  public exampleImagesBase64?: Array<string>;
+
+  constructor({ exampleImagesBase64, ...argsObjs }: ComplexImageHCaptchaRequestIn) {
     super({ type: TaskType.ComplexImageTask, _class: 'hcaptcha', ...argsObjs });
+    this.exampleImagesBase64 = exampleImagesBase64;
   }
 }
--- src/Requests/RequestsSerialization.u.spec.ts
+++ src/Requests/RequestsSerialization.u.spec.ts
@@ -366,6 +366,7 @@ describe('Check unit tests for SerializeObject()', () => {
           Task: 'Please click each image containing a mountain',
         },
         websiteURL: 'https://lessons.zennolab.com/captchas/recaptcha/v2_simple.php?level=middle',
+        exampleImagesBase64: ['https://i.postimg.cc/kg71cbRt/image-1.jpg'],
       }),
     });
 
@@ -375,6 +376,7 @@ describe('Check unit tests for SerializeObject()', () => {
         type: 'ComplexImageTask',
         class: 'hcaptcha',
         imageUrls: ['https://i.postimg.cc/kg71cbRt/image-1.jpg', 'https://i.postimg.cc/6381Zx2j/image.jpg'],
+        exampleImagesBase64: ['https://i.postimg.cc/kg71cbRt/image-1.jpg'],
         metadata: {
           Task: 'Please click each image containing a mountain',
         },
