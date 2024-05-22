dda45a857dc8c52cd605c589a8e4f0c899ef1a4d
bezzz <bezzz2304@gmail.com>
2024-05-22 13:29:39 +0300

    CCL-4354 fixes after review

--- src/Requests/AmazonProxylessRequest.ts
+++ src/Requests/AmazonProxylessRequest.ts
@@ -1,11 +1,11 @@
 import { TaskType } from '../TaskType';
-import { AmazonRequestBase, AmazonRequestBaseIn } from './AmazaonRequestBase';
+import { AmazonRequestBase, AmazonRequestBaseIn } from './AmazonRequestBase';
 
 export type AmazonProxylessRequestIn = Pick<AmazonRequestBaseIn, Exclude<keyof AmazonRequestBaseIn, 'type'>>;
 
 /**
  * Amazon recognition request (without proxy).
- * {@link https://docs.capmonster.cloud/docs/captchas/amazon-task}
+ * {@link https://zenno.link/doc-amazon}
  */
 export class AmazonProxylessRequest extends AmazonRequestBase {
   constructor(argsObj: AmazonProxylessRequestIn) {
